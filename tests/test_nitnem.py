c8976b06191ad48f901c35db4ae689f316a7070f
Sukhbinder Singh <sukhbindersingh@gmail.com>
2024-11-10 22:23:54 +0530

    Update test

--- tests/test_nitnem.py
+++ tests/test_nitnem.py
@@ -1,7 +1,18 @@
 from nitnem import cli
+import pytest
+import argparse
 
 
 def test_create_parser():
     parser = cli.create_parser()
-    result = parser.parse_args(["hello"])
-    assert result.name == "hello"
+    result = parser.parse_args([])
+    assert result.audio_choice == "auto"
+    assert result.audio_file is None
+    assert result.delay_duration == 3
+
+
+def test_create_parser_with_ac():
+    parser = cli.create_parser()
+    result = parser.parse_args(["-ac", "sukhmani"])
+    assert result.audio_choice == "sukhmani"
+    assert result.audio_file is None
